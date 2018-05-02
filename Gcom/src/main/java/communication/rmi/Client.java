abe0232f044f813e83fb09c749a29032efad8474
Karl Dackland <c14kdd@cs.umu.se>
2018-05-02 10:30:05 +0200

    second

---
+++ Gcom/src/main/java/communication/rmi/Client.java
@@ -0,0 +1,10 @@
+package communication.rmi;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Client {
+    public static void main(String args[]) {
+        System.out.println("hello world");
+    }
+}
