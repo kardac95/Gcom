52a822a28613bb39ed07f88b45e8f810c786aff5
Kristoffer Gilliusson <c14kgn@cs.umu.se>
2018-05-04 16:05:39 +0200

    More gui but changing between scenes are not working

---
+++ Gcom/src/main/java/GUI/StartController.java
@@ -0,0 +1,37 @@
+package GUI;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class StartController {
+    @FXML Button continueToGuiButton;
+
+    public StartController() {
+
+    }
+    public void changeSceneToGUI(ActionEvent event) throws IOException {
+        Stage appStage;
+        Parent root;
+        if(event.getSource()==continueToGuiButton)
+        {
+            appStage=(Stage)continueToGuiButton.getScene().getWindow();
+
+            URL url = new File("src/main/java/GUI/gui.fxml").toURL();
+            FXMLLoader loader = new FXMLLoader(url);
+            root = loader.load();
+
+            Scene scene=new Scene(root);
+            appStage.setScene(scene);
+            appStage.show();
+        }
+    }
+}
